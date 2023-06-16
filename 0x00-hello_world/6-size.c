a4d0d0c452cefb8fbf822be3f4309096ec8fdfce
Issachar <128964601+issa-char@users.noreply.github.com>
2023-06-17 01:31:46 +0300

    size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+/**
+ * main -print size of types
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of char: %c" char);
+  printf("Size of int: %i" int);
+  printf("Size of a long int: %li" long);
+  printf("Size of float: %f", float);
+  
+  return (0);
+}
