78ed355706008e1fbd6ae68c7f2856a1b9053cd7
issachar <128964601+issa-char@users.noreply.github.com>
2023-06-20 04:55:05 +0300

    while loop and printf to print all single base ten numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number = 0;
+
+    while (number < 10) {
+        printf("%d", number);
+        number++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
