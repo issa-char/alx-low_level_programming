8cbab80688b50c21cbfb04304f6b9290c4bef92a
issachar <128964601+issa-char@users.noreply.github.com>
2023-06-20 05:03:09 +0300

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    for (number = 0; number < 10; number++) {
+        putchar(number + '0');
+        if (number != 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
