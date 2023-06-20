d6d88186e92d329818383aa6e0c4ccc8e7652d82
issachar <128964601+issa-char@users.noreply.github.com>
2023-06-20 03:43:27 +0300

    added curly braces in if..else statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
     if (n > 0)
+    {
         printf("%d is positive", n);
+    }
     else if (n = 0)
+    {
         printf("%d is zero", n);
+    }
     else
+    {
         printf("%d is negative\n",n);
+    }
 	return (0);
 }
