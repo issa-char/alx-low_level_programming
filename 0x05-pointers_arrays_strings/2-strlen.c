68c5db0aa78a4e9d9d4b6b7744ec8a4bb024d95d
issachar <128964601+issa-char@users.noreply.github.com>
2023-06-28 04:44:04 +0300

    for loop to get str length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*int _strlen() - get length of string
+*@s: first operand
+*
+*Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+	inc++;
+
+	return (inc);
+}
