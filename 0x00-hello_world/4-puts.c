83e15ccd23d918f57eb0d2e6749667f9bbbff912
issachar <128964601+issa-char@users.noreply.github.com>
2023-07-21 19:14:52 +0300

    edited to conform to betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 # include<stdio.h>
+
 /**
- * main -print string
- * Description: print string
+ * main - print string to standard output
+ * 
  * Return: 0
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
