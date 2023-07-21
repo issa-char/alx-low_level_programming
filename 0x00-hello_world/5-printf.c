de413124c8d0d53d5a1a00b22461a51b26b717bd
issachar <128964601+issa-char@users.noreply.github.com>
2023-07-21 19:38:14 +0300

    added newline

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * main -print string
+ *
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
