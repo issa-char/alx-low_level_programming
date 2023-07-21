7aac9086ac4fc42f2ed069764424797e643c4693
issachar <128964601+issa-char@users.noreply.github.com>
2023-07-22 00:15:21 +0300

    immplemented unistd.h header for write to write data directly to a file descriptor, stderr descriptor 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-# include<stdio.h>
+# include<unistd.h>
 
 /**
- * main -print string
+ * main -print to standard error followed by newline
+ *
  * Return: 1
  */
 int main(void)
 {
- write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 
-return (1);
+	return (1);
 }
