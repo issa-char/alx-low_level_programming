aa41380b25823f05cad577681c76a61debd9ff1e
issachar <128964601+issa-char@users.noreply.github.com>
2023-07-22 02:14:33 +0300

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - Prints a name using a provided function.
+ * @name: Name to print.
+ * @f: Function pointer that takes a char* argument and returns void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
