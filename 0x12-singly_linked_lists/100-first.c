3a84e55cd9e96b798919319b6ca3c8bf83b942ee
issachar <128964601+issa-char@users.noreply.github.com>
2023-07-27 18:13:35 +0300

    function print string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void_attribute__(constructor)) hare(void);
+
+/**
+ * hair - print string before main
+ *
+ */
+void har(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
