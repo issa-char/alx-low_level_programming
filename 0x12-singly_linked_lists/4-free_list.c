394046ff9c63554636131a4962d000b3a988a082
issachar <128964601+issa-char@users.noreply.github.com>
2023-07-27 18:16:36 +0300

    function frees list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <lists.h>
+
+/**
+ * free_list - frees list_t list
+ * @*head: pointer to list
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+
+}
+
