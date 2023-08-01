83c3001865f1abfeb9d8b46e54af6165a5cc00ec
issachar <128964601+issa-char@users.noreply.github.com>
2023-08-01 21:23:20 +0300

    new line at end of file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-/** 
- * main - print to standardoutput
- * @argc: number of arguments
- * @argv: array of the arguments passed
- *
- * Return: 0
- */
-int main(int argc, char *agrva)
+/**
+* main - print to standardoutput
+* @argc: number of arguments
+* *@argv: array of the arguments passed
+*
+* Return: 0
+*/
+int main(int argc, char *agrv[])
 {
 	printf("%d\n", argc);
 
 	return (0);
 }
+
