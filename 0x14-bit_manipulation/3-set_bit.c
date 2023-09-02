5e94b1902a926256d860813410a40b5baa082b40
issa-char <128964601+issa-char@users.noreply.github.com>
2023-09-01 23:15:59 -0400

    fix style

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 		if (index >= 32)
 		{
-				return -1;
+			return (-1);
 		}
 
-		*N |= (1 << index);
-		return 1;
+		*n |= (1 << index);
+		return (1);
 }
 
