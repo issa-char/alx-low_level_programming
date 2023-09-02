13d8382c2637d9a7cef3c0d9dda6d6cce878895b
issa-char <128964601+issa-char@users.noreply.github.com>
2023-09-01 23:18:38 -0400

    sytle fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,16 +4,16 @@
  * get_bit - return value of bit at index
  * @n: unsigned long int
  * @index: bit to retrieve
- * 
+ *
  * Return: value at index or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 		if (index >= 32)
 		{
-				return (-1);
+			return (-1);
 		}
 
-		return (n >> index) & 1;
+		return ((n >> index) & 1);
 }
 
