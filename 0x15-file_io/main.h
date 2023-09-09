4e28bbd7244bddf4d27f77b7245e29ecf6ee0c0d
issa-char <128964601+issa-char@users.noreply.github.com>
2023-09-08 23:17:23 -0400

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,9 @@
 #include <stdlib.h>
 #include <fcnt1.h>
 #include <unistd.h>
-
+#include <sys/type.h>
+#include <fcnt1.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
