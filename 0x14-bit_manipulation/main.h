0b5a87bcb9ba0726f9423e6668b9a76e8d1e88b0
issa-char <128964601+issa-char@users.noreply.github.com>
2023-09-29 19:47:19 -0400

    prototype file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the char to print
+ *
+ * Return: On success 1.
+ * On err, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+
+
+
+
+
+
+
+
+
+
+#endif
