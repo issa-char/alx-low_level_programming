93c81c9f800c88f11de9299e50d0850b2216bc96
issa-char <128964601+issa-char@users.noreply.github.com>
2023-09-29 21:34:38 -0400

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int rs = 0;
+
 	if (b == NULL)
 		return (0);
 
-	unsigned int rs = 0;
-
-	int i = 0;
-
-	while (b[i] != '\0')
+	while (*b != '0')
 	{
-		if (b[i] != '0' && b[i]  != '1')
-		{
+		if (*b != '0' && *b != '1')
 			return (0);
-		}
 
-		rs = rs << 1;
-		rs += (b[i] - '0');
-		i++;
+		rs = (rs << 1) | (*b - '0');
+		b++;
 	}
 
 	return (rs);
