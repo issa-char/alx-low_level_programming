a4764c6762bc3747065f5757cb90b60b70a0e1dc
issa-char <128964601+issa-char@users.noreply.github.com>
2023-10-27 18:27:34 -0400

    removed whitespace

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,6 @@ int get_bit(unsigned long int n, unsigned int index)
 		unsigned long int mask = 1UL << index;
 		int bit = (n & mask) ? 1 : 0;
 
-	
 		if (index >= (sizeof(unsigned long int) * 8))
 		{
 			return (-1);
