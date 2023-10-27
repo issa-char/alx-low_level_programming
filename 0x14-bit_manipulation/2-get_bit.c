82e1e0107297546d26a1a93b75d8672f1074720a
issa-char <128964601+issa-char@users.noreply.github.com>
2023-10-27 18:15:51 -0400

    value of bits

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index
+ * @n: The number to extract
+ * @index: Index of bit to extract
+ *
+ * Return:  value/-1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+		if (index >= (sizeof(unsigned long int) * 8))
+		{
+			return (-1);
+		}
+
+		unsigned long int mask = 1UL << index;
+		int bit = (n & mask) ? 1 : 0;
+
+		return (bit);
+}
+
