8261e71afee3397975d9d735bae4dd85307a836c
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-03 21:47:43 -0400

    copying content

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
