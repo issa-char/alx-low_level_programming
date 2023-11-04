ecc4a698070d15b42dfe3cb35429061909380d95
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-03 21:26:54 -0400

    betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,34 @@
 #include "main.h"
-
+/**
+ * create_file - create a file
+ * @filename: name of file
+ * @text_content: content of file
+ *
+ * Return:  file
+ */
 int create_file(const char *filename, char *text_content)
-{
-    int fd, bytes_written;
+	{
+		int fd, bytes_written;
 
-    if (filename == NULL)
-        return (-1);
+		if (filename == NULL)
+			return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+			return (-1);
 
-    if (text_content != NULL)
-    {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		if (text_content != NULL)
+		{
+			bytes_written = write(fd, text_content, strlen(text_content));
+			if (bytes_written == -1)
+			{
+				close(fd);
+				return (-1);
+			}
+		}
 
-    close(fd);
+		close(fd);
 
-    return (1);
-}
+		return (1);
+	}
 
