d1777cd4253310f66e749556a6237197274978c9
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-03 21:10:13 -0400

    write to POSIX

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN
+#define MAIN
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+
+
+
+
+#endif
