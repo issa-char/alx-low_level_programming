686e7c753e08a042bf8cab2a69f42dede8bda891
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-03 22:38:43 -0400

    rm

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,34 +1,52 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 
-/**
-	* main - displays the information contained in the ELF header at the start of an ELF file
-	* @argc: number of arguments passed to the program
-	* @argv: array of arguments passed to the program
-	*
-	* Return: 0 on success, or an error code on failure
-	*/
-	int main(int argc, char **argv)
-	{
-		int fd, ret;
-		Elf64_Ehdr header;
+void print_error(const char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(98);
+}
 
-		if (argc != 2)
-			dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+void print_elf_header_info(Elf64_Ehdr header) {
+    printf("  Magic:   ");
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf(" %02x", header.e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             %s\n", (header.e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", (header.e_ident[EI_DATA] == ELFDATA2MSB) ? "2's complement, big endian" : "2's complement, little endian");
+    printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
+    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %d\n", header.e_type);
+    printf("  Entry point address:               0x%lx\n", header.e_entry);
+}
 
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-			dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", argv[1]), exit(98);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        print_error("Usage: elf_header elf_filename");
+    }
 
-		ret = read(fd, &header, sizeof(header));
-		if (ret != sizeof(header) || memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
-			dprintf(STDERR_FILENO, "Error: Not an ELF file - it has a different magic number.\n"), exit(98);
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        print_error("Error: Unable to open the file");
+    }
 
-		print_elf_header(&header);
+    Elf64_Ehdr header;
+    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+        print_error("Error: Unable to read ELF header");
+    }
 
-		ret = close(fd);
-		if (ret == -1)
-			dprintf(STDERR_FILENO, "Error: Cannot close fd %d\n", fd), exit(100);
+    if (!(header.e_ident[EI_MAG0] == ELFMAG0 && header.e_ident[EI_MAG1] == ELFMAG1 &&
+          header.e_ident[EI_MAG2] == ELFMAG2 && header.e_ident[EI_MAG3] == ELFMAG3)) {
+        print_error("Error: Not an ELF file");
+    }
 
-		return (0);
-	}
+    print_elf_header_info(header);
+
+    close(fd);
+    return 0;
+}
 
